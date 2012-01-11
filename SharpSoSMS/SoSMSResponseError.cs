f1a0e5e625752ca249efa432a75d02a626e3dfd3
Anderson Dias <andersondaraujo@gmail.com>
2012-01-11 10:54:04 -0300

    Melhorando implementacao de atributo

--- SharpSoSMS/SoSMSResponseError.cs
+++ SharpSoSMS/SoSMSResponseError.cs
@@ -1,41 +1,30 @@
 ﻿namespace SharpSoSMS
 {
-    /// <summary>
-    /// Contains the error message returned from SoSMS.
-    /// </summary>
-    public class SoSMSResponseError
-    {
-        private readonly string message;
+   /// <summary>
+   /// Contains the error message returned from SoSMS.
+   /// </summary>
+   public class SoSMSResponseError
+   {
+       /// <summary>
+       /// Initializes a new instance of the <see cref="SoSMSResponseError"/> class.
+       /// </summary>
+       /// <param name="message">The message.</param>
+       public SoSMSResponseError(string message)
+       {
+           this.Message = message;
+       }
 
+       public string Message { get; private set; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SoSMSResponseError"/> class.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        public SoSMSResponseError(string message)
-        {
-            this.message = message;
-        }
-
-
-        /// <summary>
-        /// Gets the error message.
-        /// </summary>
-        public string Message
-        {
-            get { return this.message; }
-        }
-
-
-        /// <summary>
-        /// Returns a <see cref="System.String"/> that represents this instance.
-        /// </summary>
-        /// <returns>
-        /// A <see cref="System.String"/> that represents this instance.
-        /// </returns>
-        public override string ToString()
-        {
-            return this.message;
-        }
-    }
+       /// <summary>
+       /// Returns a <see cref="System.String"/> that represents this instance.
+       /// </summary>
+       /// <returns>
+       /// A <see cref="System.String"/> that represents this instance.
+       /// </returns>
+       public override string ToString()
+       {
+           return this.Message;
+       }
+   }
 }
